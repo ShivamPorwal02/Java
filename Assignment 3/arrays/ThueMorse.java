822cac807d195071b95ac5c42843923c6821e22a
Shivam Porwal <porwal1234shivam@gmail.com>
2020-09-29 00:00:57 +0530

    log2 error solved

--- Assignment 3/arrays/ThueMorse.java
+++ Assignment 3/arrays/ThueMorse.java
@@ -4,13 +4,18 @@ public class ThueMorse {
         ///String[][] a = new String[n][n];
         int[] a = new int[n];
         int i = 0;
-        while (i < Math.log(n)) {
-            for (int j = 0; j < (Math.pow(2, i)); j++) {
+        // System.out.println((int) (Math.log(8) / Math.log(2)));
+        while (i < (int) (Math.log(n) / Math.log(2))) {
+            for (int j = 0; j < (int) (Math.pow(2, i)); j++) {
                 a[(int) (Math.pow(2, i)) + j] = (~a[j] & 1);
+                System.out.println(a[i] + " " + (int) (Math.pow(2, i)) + j);
             }
             i = i + 1;
         }
-        ///System.out.println();
+        for (i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
         for (i = 0; i < a.length; i++) {
             for (int j = 0; j < a.length; j++) {
                 if (a[i] == a[j]) {
