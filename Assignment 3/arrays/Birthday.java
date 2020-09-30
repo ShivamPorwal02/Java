fea7d6c28fedd5173282d62d9191c29d4a2c1cdd
Shivam Porwal <porwal1234shivam@gmail.com>
2020-09-30 15:38:25 +0530

    final

--- Assignment 3/arrays/Birthday.java
+++ Assignment 3/arrays/Birthday.java
@@ -3,7 +3,7 @@ public class Birthday {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
         int trials = Integer.parseInt(args[1]);
-        int[] a = new int[n];
+        int[] a = new int[n + 2];
         for (int i = 0; i < trials; i++) {
             boolean[] b = new boolean[n];
             int ct = 0;
@@ -26,7 +26,7 @@ public class Birthday {
             }
             double fraction = sum / trials;
             System.out.println(i + " " + a[i] + " " + fraction);
-            if (fraction   >= 0.5) break;
+            if (fraction >= 0.5) break;
         }
     }
 }
